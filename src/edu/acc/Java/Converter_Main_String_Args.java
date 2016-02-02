c280ba2f866f67b76aa012129b5a907bb4ed0b77
lewismarx <lewis.marx@g.austincc.edu>
2016-02-01 21:40:52 -0600

    2d shapes area generator. converting input to area output

--- src/edu/acc/Java/Converter_Main_String_Args.java
+++ src/edu/acc/Java/Converter_Main_String_Args.java
@@ -2,46 +2,64 @@ package edu.acc.Java;
 
 /**
  * Created by lewis on 2/1/16.
+ * @param temperature in celsius or fahrenheit
+ *
  */
 public class Converter_Main_String_Args {
 
 
     public static void main(String[] args) {
+        if(args.length == 0){
+            System.out.println("No Args Found");
+            return;
+        }
         String type = "";
         String convert = "";
         double temperature = 1;
         for (String input : args) {
             input = input.toLowerCase();
             if (input.endsWith("c")) {
-                type = " Ends With C" + "\n";
-                convert = "c";
+                type = " Ends With C" + "\n"; // sets type variable to printout with value c
+                convert = "c";  // sets String convert to "c" so that it is accessible even after string conversion to
+                                // Integer with Integer.parseInt(String st);
             }
             if (input.endsWith("f")) {
-                type = " Ends With F" + "\n";
-                convert = "f";
+                type = " Ends With F" + "\n"; // Sets type variable to value "F"
+                convert = "f";  // sets String convert to "f" so that it is accessible even after string conversion to
+                                // Integer with Integer.parseInt(String st);
             }
 
             if (input.contains("c")) {
                 input = input.replace("c", "");
                 convert = "c";
 
-
+/**
+ * Converts Fahrenheit to Celsius
+ * @params Temperature
+ * @return
+ *
+ */
             }
             if (input.contains("f")) {
                 input = input.replace("f", "");
             }
             int inputval = Integer.parseInt(input);
+            String TempConv = "";
             if (convert == "f") {
-                temperature = ((inputval - 32) / 1.8000);
+                temperature = ((inputval - 32) / 1.8000); // temperature fahrenheit to celsius conversion
+                TempConv = "c";
 
             } else if (convert == "c") {
-                temperature = (inputval * 1.8000 + 32);
+                temperature = (inputval * 1.8000 + 32); // temperature celsius to fahrenheit conversion
+                TempConv = "f";
             } else {
                 temperature = 0;
+                TempConv = "You're Stupid";
+                return;
             }
 
 
-            System.out.println(" " + inputval + type + " " + temperature);
+            System.out.println(" " + inputval + type + " " + temperature + " " + TempConv.toUpperCase());
         }
     }
 }
---
+++ src/edu/acc/Java/TWOD_Shapes.java
@@ -0,0 +1,53 @@
+package edu.acc.Java;
+
+/**
+ * Created by lewis on 2/1/16.
+ */
+public class TWOD_Shapes {
+
+
+    public static void main(String[] args) {
+
+        /*
+        Take Argument shape type, and various arguements for circle area, square area, and triangle area
+        run the dependant instance for the type with an if statement comparing the input "args" to
+        circle, square, and triangle to determine which is a viable conversion method.
+         */
+
+
+    }
+
+    public void getCirlceArea(int[] dimensions){
+
+        /*
+        Need radius or diameter / 2.  A = π * r ^ 2
+         */
+
+    }
+    public void getSquareArea(int[] dimensions){
+
+        /*
+        A=a ^ 2 where "a" is equal to one side.
+         */
+
+    }
+    public void getTriangleArea(int[] dimensions){
+
+        /*
+        c^2 = a^2 + b^2 - 2(a)(b)Math.cos(C)
+
+         */
+
+
+    }
+
+
+}
+/*
+write a shape class with  (3) instance methods
+A) getCircleArea();
+B) GetSquareArea();
+C) GetTriangleArea();
+2) Get shape type and dimension to compute area from args
+
+ */
