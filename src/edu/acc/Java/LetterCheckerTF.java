5d88985f8ea5bbb974142fc2de5e1db976ab2aad
lewismarx <lewis.marx@g.austincc.edu>
2016-02-10 19:26:59 -0600

    Classified it

---
+++ src/edu/acc/Java/LetterCheckerTF.java
@@ -0,0 +1,136 @@
+package edu.acc.Java;
+
+/**
+ * Created by lewis on 2/10/16.
+ */
+public class LetterCheckerTF {
+
+    public LetterCheckerTF(char input) {
+        isLetter(input);
+    }
+
+    boolean isLetter(char input) {
+
+
+
+        switch (input) {
+            case 'a':
+
+
+                return true;
+            case 'b':
+
+
+                return true;
+            case 'c':
+
+
+                return true;
+            case 'd':
+
+
+                return true;
+            case 'e':
+
+
+
+                return true;
+            case 'f':
+
+
+                return true;
+            case 'g':
+
+
+                return true;
+            case 'h':
+
+
+                return true;
+            case 'i':
+
+
+                return true;
+            case 'j':
+
+
+
+                return true;
+
+
+            case 'k':
+
+
+                return true;
+            case 'l':
+
+
+                return true;
+            case 'm':
+
+
+
+                return true;
+            case 'n':
+
+
+                return true;
+            case 'o':
+
+
+                return true;
+            case 'p':
+
+
+                return true;
+            case 'q':
+
+
+                return true;
+            case 'r':
+
+
+
+                return true;
+            case 's':
+
+
+                return true;
+            case 't':
+
+
+                return true;
+
+            case 'u':
+
+
+                return true;
+            case 'v':
+
+
+
+                return true;
+            case 'w':
+
+
+                return true;
+            case 'x':
+
+
+                return true;
+            case 'y':
+
+
+                return true;
+            case 'z':
+
+
+                return true;
+            default:
+
+
+                return false;
+
+        }
+    }
+}
